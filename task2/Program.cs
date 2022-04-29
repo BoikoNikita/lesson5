66d6c6487f98a5f00d39b4325ad61c5d1bf5aab2
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-29 21:33:35 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,32 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index<length)
+    {
+        collection[index] = new Random().Next(0,10);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[6];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+var even = array.Where((element, index) => index % 2 != 0);
+Console.WriteLine("Сумма чисел на четных местах: {0}", even.Sum());
